422dfb06b93b3d1fb537debe3b63908fb15dfbd7
gudkovWork@gmail.com <gudkovWork@gmail.com@b55020a6-0b5b-d1b9-7a2b-1cc2611a2dcf>
2013-12-01 14:46:49 +0000

    Added more SubjectDao methods and their implementations

--- trunk/ORM/src/main/java/com/tsystems/orm/dal/SubjectDao.java
+++ trunk/ORM/src/main/java/com/tsystems/orm/dal/SubjectDao.java
@@ -8,5 +8,8 @@ public interface SubjectDao extends GenericDao<Subject,Integer>{
 
 	public List<Subject> getAllSubjects();
 	public Subject getMostPopularSubject();
+	public Subject getSubjectByID(int id);
+	public void createSubject(Subject subject);
+	public void changeSubjectName(Subject subject, String name);
 	
 }
--- trunk/ORM/src/main/java/com/tsystems/orm/util/HibernateUtils.java
+++ trunk/ORM/src/main/java/com/tsystems/orm/util/HibernateUtils.java
@@ -10,46 +10,47 @@ import org.hibernate.service.ServiceRegistryBuilder;
 public class HibernateUtils {
 
 	private static final SessionFactory sessionFactory;
-	//Hibernate 4
-	
+	// Hibernate 4
+
 	private static ServiceRegistry serviceRegistry;
-	
+
 	static {
 		try {
-		    Configuration configuration = new Configuration();
-		    //look here what happends!!!
-		    configuration.configure("/hibernate.cfg.xml" );
-		    serviceRegistry = new ServiceRegistryBuilder().applySettings(configuration.getProperties()).buildServiceRegistry();        
-		    sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+			Configuration configuration = new Configuration();
+			// look here what happends!!!
+			configuration.configure("/hibernate.cfg.xml");
+			serviceRegistry = new ServiceRegistryBuilder().applySettings(
+					configuration.getProperties()).buildServiceRegistry();
+			sessionFactory = configuration.buildSessionFactory(serviceRegistry);
 		} catch (HibernateException ex) {
 			System.err.println("Initial SessionFactory creation failed." + ex);
 			throw new ExceptionInInitializerError(ex);
 		}
-	}  
-	 
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-	
+	}
+
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+
 	public static Session getSession() {
 		Session hibernateSession = sessionFactory.getCurrentSession();
 		return hibernateSession;
 	}
-	
-    public static void beginTransaction() {
+
+	public static void beginTransaction() {
 		HibernateUtils.getSession().getTransaction().begin();
 	}
-	 
+
 	public static void commitTransaction() {
 		HibernateUtils.getSession().getTransaction().commit();
 	}
-	 
+
 	public static void rollbackTransaction() {
 		HibernateUtils.getSession().getTransaction().rollback();
 	}
-	 
+
 	public static void closeSession() {
 		HibernateUtils.getSession().close();
 	}
-	
+
 }
